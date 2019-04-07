21ae4340fe4fa1fa883f3d3ff19b08f3a2ff44d5
Song <shg9411@naver.com>
2019-04-07 18:55:05 +0900

    no message

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    # auto_now는 수정일자, auto_now_add는 최초 생성일자
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
