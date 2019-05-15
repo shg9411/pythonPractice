5d04c604bb13b235f8f5c7dd0da4483de02d5870
Song_H_G <shg9411@naver.com>
2019-05-15 20:09:14 +0900

    no message

--- django_shop/myshop/payment/admin.py
+++ django_shop/myshop/payment/admin.py
@@ -1,23 +1,3 @@
-import csv
-import datetime
-from django.http import HttpResponse
+from django.contrib import admin
 
-
-def export_to_csv(modeladmin, request, queryset):
-    opts = modeladmin.model._meta
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = f'attachment; filename={opts.verbose_name}.csv'
-    writer = csv.writer(response)
-
-    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
-    writer.writerow([field.verbose_name for field in fields])
-    for obj in queryset:
-        data_row = []
-        for field in fields:
-            value = getattr(obj, field.name)
-            if isinstance(value, datetime.datetime):
-                value = value.strftime('%d/%m/%Y')
-            data_row.append(value)
-        writer.writerow(data_row)
-    return response
-export_to_csv.short_description = 'Export to CSV'
+# Register your models here.
